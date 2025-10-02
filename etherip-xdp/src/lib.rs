af729948dee8be15e43583c7748ccc40925d32eb
metastable-void <git@yuka.h09.eu>
2025-10-02 19:08:13 +0000

    refactoring and hardening

--- etherip-xdp/src/lib.rs
+++ etherip-xdp/src/lib.rs
@@ -1,4 +1,4 @@
-use anyhow::{bail, Context as _};
+use anyhow::{anyhow, bail, Context as _};
 use aya::programs::{Xdp, XdpFlags};
 use clap::Parser;
 #[rustfmt::skip]
@@ -7,12 +7,13 @@ use tokio::signal;
 
 use core::net::Ipv6Addr;
 use std::collections::HashSet;
-use std::net::SocketAddr;
+use std::io::{self, ErrorKind};
+use std::net::{IpAddr, SocketAddr};
 
 use etherip_xdp_common::{iface, ipv6, mac, vlan};
+use ftth_rtnl::{AddressScope, Ipv6Net, RtnlClient};
 
 pub mod interface;
-pub mod rtnl;
 
 #[derive(Debug, Parser)]
 #[clap(rename_all = "kebab-case")]
@@ -73,6 +74,16 @@ impl RemoteAddr {
     }
 }
 
+async fn call_blocking_io<F, R>(f: F) -> io::Result<R>
+where
+    F: FnOnce() -> io::Result<R> + Send + 'static,
+    R: Send + 'static,
+{
+    tokio::task::spawn_blocking(f)
+        .await
+        .map_err(|err| io::Error::new(ErrorKind::Other, err))?
+}
+
 #[derive(Debug, Clone)]
 struct TunnelConfig {
     vlan_id: u16,
@@ -102,7 +113,10 @@ impl TunnelSpec {
             }
 
             let mut kv = part.splitn(2, '=');
-            let key = kv.next().unwrap().trim();
+            let key = kv
+                .next()
+                .map(str::trim)
+                .ok_or_else(|| "malformed tunnel definition".to_string())?;
             if let Some(value) = kv.next() {
                 let value = value.trim();
                 match key {
@@ -204,27 +218,17 @@ pub async fn run(opt: Opt) -> anyhow::Result<()> {
         warn!("failed to initialize eBPF logger: {}", e);
     }
 
-    let bind_iface_index = interface::name_to_index(&bind_iface)
-        .expect("failed to get interface index")
-        .inner_unchecked();
-    if bind_iface_index == 0 {
-        return Err(anyhow::anyhow!(
-            "failed to get interface index for {}: {}",
-            bind_iface,
-            std::io::Error::last_os_error()
-        ));
-    }
+    let bind_iface_id = interface::name_to_index(&bind_iface)
+        .with_context(|| format!("failed to get interface index for {bind_iface}"))?;
+    let bind_iface_index = bind_iface_id
+        .inner()
+        .ok_or_else(|| anyhow!("interface index for {bind_iface} is unspecified"))?;
 
-    let bridged_iface_index = interface::name_to_index(&bridged_iface)
-        .expect("failed to get interface index")
-        .inner_unchecked();
-    if bridged_iface_index == 0 {
-        return Err(anyhow::anyhow!(
-            "failed to get interface index for {}: {}",
-            bridged_iface,
-            std::io::Error::last_os_error()
-        ));
-    }
+    let bridged_iface_id = interface::name_to_index(&bridged_iface)
+        .with_context(|| format!("failed to get interface index for {bridged_iface}"))?;
+    let bridged_iface_index = bridged_iface_id
+        .inner()
+        .ok_or_else(|| anyhow!("interface index for {bridged_iface} is unspecified"))?;
 
     let mut tunnel_configs = Vec::with_capacity(tunnels.len());
     let mut seen_vlans = HashSet::with_capacity(tunnels.len());
@@ -243,85 +247,197 @@ pub async fn run(opt: Opt) -> anyhow::Result<()> {
 
     let tunnels = tunnel_configs;
 
-    let encap_program: &mut Xdp = ebpf.program_mut("etherip_xdp_encap").unwrap().try_into()?;
+    let encap_program: &mut Xdp = ebpf
+        .program_mut("etherip_xdp_encap")
+        .ok_or_else(|| anyhow!("eBPF program etherip_xdp_encap not found"))?
+        .try_into()?;
     encap_program.load()?;
     encap_program.attach(&bridged_iface, XdpFlags::default())
         .context("failed to attach the XDP program with default flags - try changing XdpFlags::default() to XdpFlags::SKB_MODE")?;
 
-    let decap_program: &mut Xdp = ebpf.program_mut("etherip_xdp_decap").unwrap().try_into()?;
+    let decap_program: &mut Xdp = ebpf
+        .program_mut("etherip_xdp_decap")
+        .ok_or_else(|| anyhow!("eBPF program etherip_xdp_decap not found"))?
+        .try_into()?;
     decap_program.load()?;
     decap_program.attach(&bind_iface, XdpFlags::default())
         .context("failed to attach the XDP program with default flags - try changing XdpFlags::default() to XdpFlags::SKB_MODE")?;
 
-    let mut if_indexes =
-        aya::maps::HashMap::<_, u32, u32>::try_from(ebpf.map_mut("IF_INDEX_MAP").unwrap())?;
+    let mut if_indexes = aya::maps::HashMap::<_, u32, u32>::try_from(
+        ebpf.map_mut("IF_INDEX_MAP")
+            .ok_or_else(|| anyhow!("IF_INDEX_MAP not found"))?,
+    )?;
 
     if_indexes.insert(iface::IF_INDEX_INNER, bridged_iface_index, 0)?;
     if_indexes.insert(iface::IF_INDEX_OUTER, bind_iface_index, 0)?;
 
     info!("XDP program loaded and attached to interfaces");
 
-    let rtnl_conn = rtnl::RtnetlinkConnection::new().await?;
-    let mut link_manager = rtnl_conn.link();
-    let addr_manager = rtnl_conn.address();
-    let route_manager = rtnl_conn.route();
-    let neigh_manager = rtnl_conn.neighbor();
+    let rtnl = RtnlClient::new();
+    let link_client = rtnl.link();
+    let address_client = rtnl.address();
+    let route_client = rtnl.route();
+    let neighbor_client = rtnl.neighbor();
+    let default_ipv6_route_prefix =
+        Ipv6Net::new(Ipv6Addr::UNSPECIFIED, 0).map_err(|err| anyhow!(err))?;
+    let tunnels_task = tunnels.clone();
+    let bind_iface_task = bind_iface.clone();
+    let bind_iface_id_task = bind_iface_id;
 
     tokio::spawn(async move {
+        let mut ebpf = ebpf;
+        let tunnels = tunnels_task;
+        let bind_iface = bind_iface_task;
+        let address_client = address_client;
+        let route_client = route_client;
+        let neighbor_client = neighbor_client;
+        let link_client = link_client;
+        let default_route_prefix = default_ipv6_route_prefix;
+        let bind_iface_id = bind_iface_id_task;
+
         loop {
             tokio::time::sleep(tokio::time::Duration::from_secs(1)).await;
 
-            // Insert the local IPv6 address into the map
-            let local_if_id = interface::InterfaceId::new(bind_iface_index);
-            let local_addr = addr_manager
-                .get_v6(local_if_id, rtnl::addr::V6AddressRequestScope::Global)
-                .await?;
-            if local_addr.is_empty() {
+            let local_if_id = bind_iface_id;
+            let Some(local_if_index) = local_if_id.inner() else {
+                log::error!("local interface index became unspecified");
+                continue;
+            };
+
+            let local_addrs = call_blocking_io({
+                let address_client = address_client.clone();
+                move || {
+                    address_client.ipv6_addrs_get_with_scope(
+                        Some(local_if_index),
+                        Some(AddressScope::Universe),
+                    )
+                }
+            })
+            .await?;
+            if local_addrs.is_empty() {
                 log::error!("No global IPv6 address found for interface {}", bind_iface);
                 continue;
             }
-            let local_addr = local_addr[0];
+            let local_addr = local_addrs[0];
             let local_addr = ipv6::to_u128(local_addr.octets());
             let mut ipv6_addr_map = aya::maps::HashMap::<_, u16, u128>::try_from(
-                ebpf.map_mut("IPV6_ADDR_MAP").unwrap(),
+                ebpf.map_mut("IPV6_ADDR_MAP")
+                    .ok_or_else(|| anyhow!("IPV6_ADDR_MAP not found"))?,
             )?;
             ipv6_addr_map.insert(vlan::VLAN_ID_LOCAL, local_addr, 0)?;
 
-            // Get the IPv6 gateway
-            let route = route_manager.get_v6(Ipv6Addr::UNSPECIFIED, 0).await?;
-            if route.is_empty() {
-                log::error!("No IPv6 route found");
-                continue;
-            }
-            let (egress_if_id, gateway_addr) = route[0];
+            let route = match call_blocking_io({
+                let route_client = route_client.clone();
+                move || route_client.ipv6_route_get_by_prefix(default_route_prefix)
+            })
+            .await
+            {
+                Ok(route) => route,
+                Err(err) if err.kind() == ErrorKind::NotFound => {
+                    log::error!("No IPv6 route found");
+                    continue;
+                }
+                Err(err) => {
+                    log::error!("Failed to retrieve IPv6 default route: {}", err);
+                    continue;
+                }
+            };
+
+            let egress_if_index = route
+                .if_id
+                .or_else(|| route.nexthops.iter().find_map(|hop| hop.if_id));
+            let egress_if_index = match egress_if_index {
+                Some(index) if index != 0 => index,
+                _ => {
+                    log::error!("No egress interface found for default IPv6 route");
+                    continue;
+                }
+            };
+            let gateway_addr = route
+                .gateway
+                .and_then(|gw| match gw {
+                    IpAddr::V6(addr) => Some(addr),
+                    _ => None,
+                })
+                .or_else(|| {
+                    route.nexthops.iter().find_map(|hop| {
+                        hop.gateway.and_then(|gw| match gw {
+                            IpAddr::V6(addr) => Some(addr),
+                            _ => None,
+                        })
+                    })
+                });
+            let gateway_addr = match gateway_addr {
+                Some(addr) => addr,
+                None => {
+                    log::error!("No IPv6 gateway found for default route");
+                    continue;
+                }
+            };
             log::debug!(
                 "Egress interface ID: {}, Gateway address: {}",
-                egress_if_id.inner_unchecked(),
+                egress_if_index,
                 gateway_addr
             );
 
-            let lladdr = neigh_manager
-                .neigh_get(egress_if_id, std::net::IpAddr::V6(gateway_addr))
-                .await?;
-            if lladdr.is_none() {
-                log::error!("No link layer address found for gateway {}", gateway_addr);
-                continue;
-            }
-            let lladdr = lladdr.unwrap();
-            log::debug!("Gateway's link layer address: {:?}", lladdr);
-            let lladdr = mac::to_u64((&lladdr as &[u8]).try_into().unwrap());
+            let gateway_entry = match call_blocking_io({
+                let neighbor_client = neighbor_client.clone();
+                move || neighbor_client.get(IpAddr::V6(gateway_addr), Some(egress_if_index))
+            })
+            .await
+            {
+                Ok(entry) => entry,
+                Err(err) if err.kind() == ErrorKind::NotFound => {
+                    log::error!("No link layer address found for gateway {}", gateway_addr);
+                    continue;
+                }
+                Err(err) => {
+                    log::error!(
+                        "Failed to resolve neighbor entry for gateway {}: {}",
+                        gateway_addr,
+                        err
+                    );
+                    continue;
+                }
+            };
 
-            // Insert the MAC address of the local interface into the map
-            let local_mac = link_manager.get_link_layer_address(local_if_id).await?;
-            if local_mac.is_none() {
-                log::error!("No MAC address found for interface {}", bind_iface);
-                continue;
-            }
-            let local_mac = local_mac.unwrap();
+            let lladdr_bytes: [u8; 6] = match gateway_entry.link_address {
+                Some(addr) => match addr.as_slice().try_into() {
+                    Ok(bytes) => bytes,
+                    Err(_) => {
+                        log::error!(
+                            "Unexpected link layer address length for gateway {}",
+                            gateway_addr
+                        );
+                        continue;
+                    }
+                },
+                None => {
+                    log::error!("No link layer address found for gateway {}", gateway_addr);
+                    continue;
+                }
+            };
+            log::debug!("Gateway's link layer address: {:?}", lladdr_bytes);
+            let lladdr = mac::to_u64(&lladdr_bytes);
+
+            let local_mac = call_blocking_io({
+                let link_client = link_client.clone();
+                move || link_client.mac_addr_get(local_if_index)
+            })
+            .await?;
+            let local_mac = match local_mac {
+                Some(mac) => mac.inner,
+                None => {
+                    log::error!("No MAC address found for interface {}", bind_iface);
+                    continue;
+                }
+            };
             log::debug!("Local MAC address: {:?}", local_mac);
-            let local_mac = mac::to_u64((&local_mac as &[u8]).try_into().unwrap());
-            let mut mac_addr_map =
-                aya::maps::HashMap::<_, u32, u64>::try_from(ebpf.map_mut("MAC_ADDR_MAP").unwrap())?;
+            let local_mac = mac::to_u64(&local_mac);
+            let mut mac_addr_map = aya::maps::HashMap::<_, u32, u64>::try_from(
+                ebpf.map_mut("MAC_ADDR_MAP")
+                    .ok_or_else(|| anyhow!("MAC_ADDR_MAP not found"))?,
+            )?;
             mac_addr_map.insert(mac::MAC_ADDR_LOCAL, local_mac, 0)?;
             mac_addr_map.insert(mac::MAC_ADDR_GATEWAY, lladdr, 0)?;
 
@@ -343,11 +459,13 @@ pub async fn run(opt: Opt) -> anyhow::Result<()> {
                     Ipv6Addr::from(addr)
                 );
                 let mut ipv6_addr_map = aya::maps::HashMap::<_, u16, u128>::try_from(
-                    ebpf.map_mut("IPV6_ADDR_MAP").unwrap(),
+                    ebpf.map_mut("IPV6_ADDR_MAP")
+                        .ok_or_else(|| anyhow!("IPV6_ADDR_MAP not found"))?,
                 )?;
                 ipv6_addr_map.insert(tunnel.vlan_id, addr, 0)?;
                 let mut vlan_id_map = aya::maps::HashMap::<_, u128, u16>::try_from(
-                    ebpf.map_mut("VLAN_ID_MAP").unwrap(),
+                    ebpf.map_mut("VLAN_ID_MAP")
+                        .ok_or_else(|| anyhow!("VLAN_ID_MAP not found"))?,
                 )?;
                 vlan_id_map.insert(addr, tunnel.vlan_id, 0)?;
             }
